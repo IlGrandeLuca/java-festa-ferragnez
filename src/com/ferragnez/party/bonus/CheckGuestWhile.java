90522240fe154387f018c871806999ce37bd18c3
IlGrandeLuca <lucailgrande96@outlook.it>
2021-11-19 14:51:18 +0100

    Create CheckGuestWhile.java

---
+++ src/com/ferragnez/party/bonus/CheckGuestWhile.java
@@ -0,0 +1,41 @@
+package com.ferragnez.party.bonus;
+
+import java.util.Scanner;
+
+public class CheckGuestWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner bouncer = new Scanner(System.in);
+		String[] guestList = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+		boolean checked = false;
+		int counter = 0;
+		
+		System.out.print("Enter guest name: ");	
+		String guestName = bouncer.nextLine();
+		System.out.print("Search for " + guestName + "...\n");
+		
+		while(!checked && counter < guestList.length) {
+			
+			guestList[counter] = guestList[counter].toLowerCase();
+			guestName = guestName.toLowerCase();	
+			
+			if(guestList[counter].equals(guestName)) {
+				
+				System.out.println(guestName + " can now enter.");
+				checked = true;
+			} else {
+				
+				counter++;
+			}
+		}
+		
+		if(!checked) {
+			
+			System.out.println("Go away!");
+		}
+		
+		bouncer.close();
+	}
+
+}
